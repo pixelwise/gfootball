7aad817dcc4cf6113741dae27533107a60008a2f
Artem Zapara <art.zapara@gmail.com>
2026-03-10 12:14:03 +0100

    add bash scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 -m pip install . \
+ --use-pep517 --no-build-isolation  # see https://github.com/pypa/pip/issues/6334
---
+++ start_game.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m gfootball.play_game \
+ --config_file gfootball/configs/default.yaml \
+ --render=true
